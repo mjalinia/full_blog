f5ca112a9c9551fd9db8f4d252eb43eccd5116ab
mjalinia <mtalinia988@gmail.com>
2018-02-06 21:20:49 +0330

    first commit

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView
-from django.views.generic import DetailView
-
+from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import UpdateView,DeleteView
+from django.urls import reverse_lazy
 from . models import Post
 
 
@@ -13,3 +13,18 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'detail.html'
     context_object_name = 'bbin'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'create.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body',]
+    template_name = 'post_edit.html'
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name='post_delete.html'
+    success_url = reverse_lazy('home')
